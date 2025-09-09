939c380dbf78921264ae53ad7b11fb39a728f922
wonjun4149 <wonjun4149@gmail.com>
2025-09-09 16:50:25 +0900

    meeting_navbar 수정

--- php/meeting.php
+++ php/meeting.php
@@ -43,22 +43,31 @@ try {
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <nav class="navbar">
+   <nav class="navbar">
         <div class="nav-container">
             <div class="nav-left">
-                <div class="hamburger"><span></span><span></span><span></span></div>
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
                 <div class="logo"><a href="../index.php">MOIT</a></div>
                 <ul class="nav-menu">
-                    <li><a href="introduction.php">소개</a></li>
+                    <li><a href="introduction.php" class="active">소개</a></li>
                     <li><a href="hobby_recommendation.php">취미 추천</a></li>
-                    <li><a href="meeting.php" class="active">모임</a></li>
+                    <li><a href="meeting.php">모임</a></li>
                 </ul>
             </div>
-           <div class="nav-right">
-                <span class="welcome-msg">환영합니다, <?php echo htmlspecialchars($_SESSION['user_nickname']); ?>님!</span>
-                <a href="mypage.php" class="nav-btn">마이페이지</a>
-                <a href="logout.php" class="nav-btn logout-btn">로그아웃</a>
-                <button class="profile-btn"></button>
+            <div class="nav-right">
+                <?php if (isLoggedIn()): ?>
+                    <span class="welcome-msg">환영합니다, <?php echo htmlspecialchars($_SESSION['user_nickname']); ?>님!</span>
+                    <a href="mypage.php" class="nav-btn">마이페이지</a> <a href="logout.php" class="nav-btn logout-btn">로그아웃</a>
+                    <button class="profile-btn"></button>
+                <?php else: ?>
+                    <a href="login.php" class="nav-btn">로그인</a>
+                    <a href="register.php" class="nav-btn">회원가입</a>
+                    <button class="profile-btn"></button>
+                <?php endif; ?>
             </div>
         </div>
     </nav>
